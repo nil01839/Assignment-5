434f428879fe6ce7a462f12d7e7a6c8253092268
nil01839 <nil01839@gmail.com>
2024-01-05 19:19:00 +0600

    js done with function

--- js/app.js
+++ js/app.js
@@ -1,256 +1,114 @@
-console.log('Done!');
-document.getElementById('player-one').addEventListener('click', function () {
-
+function selectPlayer(playersName, btnId) {
     const pyalerContainer = document.getElementById('player-Container');
     const children = pyalerContainer.children;
     const playerNumber = children.length;
 
     if (playerNumber == 5) {
         alert('not more than 5')
-        // return;
+
 
     }
     else {
         const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-one-name');
+        const playerName = document.getElementById(playersName);
         const player = playerName.innerText;
         const newLi = document.createElement("li");
         newLi.innerText = player;
         playerContainer.appendChild(newLi);
 
-
-    }
-    const sButton = document.getElementById('player-one');
-    sButton.disabled = true;
-    sButton.style.background = '#8DF29B';
-
-})
-
-
-document.getElementById('player-two').addEventListener('click', function () {
-
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-two-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-two');
+        const sButton = document.getElementById(btnId);
         sButton.disabled = true;
         sButton.style.background = '#8DF29B';
+
+
     }
+}
+
+// function selectedBtnDisable(btnId) {
+//     const sButton = document.getElementById(btnId);
+//     sButton.disabled = true;
+//     sButton.style.background = '#8DF29B';
+
+// }
+
+document.getElementById('player-one').addEventListener('click', function () {
+
+    selectPlayer('player-one-name', 'player-one');
+    // selectedBtnDisable('player-one');
+
+})
+document.getElementById('player-two').addEventListener('click', function () {
+
+    selectPlayer('player-two-name', 'player-two');
+    // selectedBtnDisable('player-two');
 
 })
 document.getElementById('player-three').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-three-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-three');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
+    selectPlayer('player-three-name', 'player-three');
+    // selectedBtnDisable('player-three');
 
 })
 document.getElementById('player-four').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-four-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-four');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
+    selectPlayer('player-four-name', 'player-four');
+    // selectedBtnDisable('player-four');
 
 })
 document.getElementById('player-five').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-five-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-five');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
+    selectPlayer('player-five-name', 'player-five');
+    // selectedBtnDisable('player-five');
 
 })
 document.getElementById('player-six').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-six-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-six');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
-
+    selectPlayer('player-six-name', 'player-six');
+    // selectedBtnDisable('player-six');
 
 })
 document.getElementById('player-seven').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-seven-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-seven');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
+    selectPlayer('player-seven-name', 'player-seven');
+    // selectedBtnDisable('player-seven');
 
 })
 document.getElementById('player-eight').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-eight-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-eight');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
+    selectPlayer('player-eight-name', 'player-eight');
+    // selectedBtnDisable('player-eight');
 
 })
 document.getElementById('player-nine').addEventListener('click', function () {
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-
-    if (playerNumber == 5) {
-        alert('not more than 5')
-        return;
-
-    }
-    else {
-        const playerContainer = document.getElementById('player-Container');
-        const playerName = document.getElementById('player-nine-name');
-        const player = playerName.innerText;
-        const newLi = document.createElement("li");
-        newLi.innerText = player;
-        playerContainer.appendChild(newLi);
-
-        const sButton = document.getElementById('player-nine');
-        sButton.disabled = true;
-        sButton.style.background = '#8DF29B';
-    }
-
+    selectPlayer('player-nine-name', 'player-nine');
+    // selectedBtnDisable('player-nine');
 
 })
+
 
 document.getElementById('claculate-btn').addEventListener('click', function () {
     const perPlayerCostElment = document.getElementById('per-player-cost');
     const perPlayerCostString = perPlayerCostElment.value;
     const perPlayerCost = parseFloat(perPlayerCostString);
 
-    const playerExpenceElement = document.getElementById('player-expence');
+    if (isNaN(perPlayerCost)) {
+        alert('Please enter a number!');
+        return;
+    }
 
-    const pyalerContainer = document.getElementById('player-Container');
-    const children = pyalerContainer.children;
-    const playerNumber = children.length;
-    // console.log(playerNumber);
+    else {
+        const playerExpenceElement = document.getElementById('player-expence');
 
-    const totalPlayerExpence = perPlayerCost * playerNumber;
-    playerExpenceElement.innerText = totalPlayerExpence;
-    perPlayerCostElment.value = '';
+        const pyalerContainer = document.getElementById('player-Container');
+        const children = pyalerContainer.children;
+        const playerNumber = children.length;
+        // console.log(playerNumber);
+
+        const totalPlayerExpence = perPlayerCost * playerNumber;
+        playerExpenceElement.innerText = totalPlayerExpence;
+        perPlayerCostElment.value = '';
+    }
 
 
 })
@@ -268,23 +126,25 @@ document.getElementById('total-btn').addEventListener('click', function () {
     const coachCostString = coachCostElement.value;
     const coachCost = parseFloat(coachCostString);
 
-    const totalCostElement = document.getElementById('total-cost');
-    const totalCost = playerExpence + managerCost + coachCost;
-    totalCostElement.innerText = totalCost;
+    if (isNaN(managerCost && coachCost)) {
+        alert('Please enter a number!');
+        return;
+    }
 
-    managerCostElement.value = '';
-    coachCostElement.value = '';
+    else {
+        const totalCostElement = document.getElementById('total-cost');
+        const totalCost = playerExpence + managerCost + coachCost;
+        totalCostElement.innerText = totalCost;
 
-    const playerExpenceElement = document.getElementById('player-expence');
-    playerExpenceElement.innerText = '00';
+        managerCostElement.value = '';
+        coachCostElement.value = '';
 
-    console.log(perPlayerCost);
+        const playerExpenceElement = document.getElementById('player-expence');
+        playerExpenceElement.innerText = '00';
+    }
+
+    // console.log(perPlayerCost);
 
 })
-
-// const pyalerContainer = document.getElementById('player-Container');
-// const children = pyalerContainer.children;
-// const playerNumber = children.length;
-// console.log(playerNumber);
 
 
